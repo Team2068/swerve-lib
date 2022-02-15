7cb9ef9022768737889ea6a01b00342ab60699ab
keiraSchoolcraft <43482081+keiraSchoolcraft@users.noreply.github.com>
2022-02-15 17:08:50 -0500

    make so it stays absolute

--- src/main/java/com/swervedrivespecialties/swervelib/ctre/MagEncoderFactoryBuilder.java
+++ src/main/java/com/swervedrivespecialties/swervelib/ctre/MagEncoderFactoryBuilder.java
@@ -36,6 +36,9 @@ public class MagEncoderFactoryBuilder {
 
         @Override
         public double getAbsoluteAngle() {
+            if(encoder.get() > 1 || encoder.get() < 0) {
+                encoder.reset(); // clamp it to 1 at max and 0 at min
+            }
             double angle = (encoder.get() - encoder.getPositionOffset()) * Math.PI * 2; // Absolute value from 0-1 mapped from 0-2pi
             angle %= 2.0 * Math.PI;
             if (angle < 0.0) {
